cc3c293ca81994491ccbbde7e0aedfe339bab0fa
NICOLEMS99 <nicole.moura.santos@hotmail.com.br>
2023-03-29 22:42:59 -0300

    projeto_gorjeta

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+    }
+}
